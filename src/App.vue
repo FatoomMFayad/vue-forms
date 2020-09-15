eaa99804f772777d6ad345d896b37a17e9e5c448
FatoomMFayad <fatoomfayyad@gmail.com>
2020-09-15 18:50:18 +0300

    using radio buttons

--- src/App.vue
+++ src/App.vue
@@ -71,13 +71,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -116,7 +118,7 @@
                         <ul>
                             <li v-for="mail in sentMail">{{ mail }}</li>
                         </ul>
-                        <p>Gender:</p>
+                        <p>Gender:{{ gender }}</p>
                         <p>Priority:</p>
                         <p>Switched:</p>
                     </div>
@@ -136,7 +138,8 @@
                     age: 32
                 },
                 message: 'Short Message',
-                sentMail: []
+                sentMail: [],
+                gender: 'Female'
 
                 
             }
